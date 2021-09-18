5b60143d2f69ec1e9b2405867b5982b249cf39d2
Anders Jurisoo <jurisoo@hotmail.com>
2021-09-18 10:50:03 +0200

    Cleanup

--- game.js
+++ game.js
@@ -40,7 +40,7 @@ loadSprite('blue-steel', 'gqVoI2b.png')
 loadSprite('blue-evil-shroom', 'SvV4ueD.png')
 loadSprite('blue-surprise', 'RMqCc1G.png')
 
-scene("game", ({ level, score }) => {
+scene("game", () => {
   layers(['bg', 'obj', 'ui'], 'obj')
 
 	const mapLength = 250
@@ -51,30 +51,28 @@ scene("game", ({ level, score }) => {
 		}).join("")
 	}
 
-  const maps = [
-    [
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			ground(0.1),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			ground(0.3),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			ground(0.5),
-			' '.repeat(mapLength),
-			' '.repeat(mapLength),
-			ground(0.7),						
-    ],
-  ]
+  const map = [
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		ground(0.1),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		ground(0.3),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		ground(0.5),
+		' '.repeat(mapLength),
+		' '.repeat(mapLength),
+		ground(0.7),						
+	]
 
-  const levelCfg = {
+  const mapCfg = {
     width: 20,
     height: 20,
     '=': [sprite('block'), solid()],
@@ -96,101 +94,19 @@ scene("game", ({ level, score }) => {
 
   }
 
-  const gameLevel = addLevel(maps[level], levelCfg)
-
-  const scoreLabel = add([
-    text(score),
-    pos(30, 6),
-    layer('ui'),
-    {
-      value: score,
-    }
-  ])
-
-  add([text('level ' + parseInt(level + 1) ), pos(40, 6)])
-  
-  function big() {
-    let timer = 0
-    let isBig = false
-    return {
-      update() {
-        if (isBig) {
-          CURRENT_JUMP_FORCE = BIG_JUMP_FORCE
-          timer -= dt()
-          if (timer <= 0) {
-            this.smallify()
-          }
-        }
-      },
-      isBig() {
-        return isBig
-      },
-      smallify() {
-        this.scale = vec2(1)
-        CURRENT_JUMP_FORCE = JUMP_FORCE
-        timer = 0
-        isBig = false
-      },
-      biggify(time) {
-        this.scale = vec2(2)
-        timer = time
-        isBig = true     
-      }
-    }
-  }
+  const gameLevel = addLevel(map, mapCfg)
 
   const player = add([
     sprite('mario'), solid(),
     pos(30, 0),
     body(),
-    big(),
     origin('bot')
   ])
-
-  action('mushroom', (m) => {
-    m.move(20, 0)
-  })
-
-  player.on("headbump", (obj) => {
-    if (obj.is('coin-surprise')) {
-      gameLevel.spawn('$', obj.gridPos.sub(0, 1))
-      destroy(obj)
-      gameLevel.spawn('}', obj.gridPos.sub(0,0))
-    }
-    if (obj.is('mushroom-surprise')) {
-      gameLevel.spawn('#', obj.gridPos.sub(0, 1))
-      destroy(obj)
-      gameLevel.spawn('}', obj.gridPos.sub(0,0))
-    }
-  })
-
-  player.collides('mushroom', (m) => {
-    destroy(m)
-    player.biggify(6)
-  })
-
-  player.collides('coin', (c) => {
-    destroy(c)
-    scoreLabel.value++
-    scoreLabel.text = scoreLabel.value
-  })
-
-  action('dangerous', (d) => {
-    d.move(-ENEMY_SPEED, 0)
-  })
-
-  player.collides('dangerous', (d) => {
-    if (isJumping) {
-      destroy(d)
-    } else {
-      go('lose', { score: scoreLabel.value})
-    }
-  })
 
   player.action(() => {
     camPos(player.pos)
     if (player.pos.y >= FALL_DEATH) {
-      go('lose', { score: scoreLabel.value})
+      go('lose')
     }
   })
 
@@ -216,8 +132,14 @@ scene("game", ({ level, score }) => {
   })
 })
 
-scene('lose', ({ score }) => {
-  add([text(score, 32), origin('center'), pos(width()/2, height()/ 2)])
+scene('lose', () => {
+  add([text("Game over", 32), origin('center'), pos(width()/2, height()/ 2)])
 })
 
-start("game", { level: 0, score: 0})
+start("game")
+
+setInterval(() => {
+	// ????
+	var canvas = document.getElementsByTagName("canvas")[0];
+	canvas.dispatchEvent(new KeyboardEvent('keypress', {'code': 39}));	
+}, 1)
